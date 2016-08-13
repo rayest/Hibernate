ad351d3d23a069f021e031af4d5c79b284cfa52a
Rayest <yi@rayest.cn>
2016-08-13 08:36:25 +0800

    封装 HibernateUtil，实现 XML 配置方式的 CRUD

--- src/test/java/cn/ipay/model/StudentTest.java
+++ src/test/java/cn/ipay/model/StudentTest.java
@@ -1,31 +1,67 @@
 package cn.ipay.model;
 
+import cn.ipay.util.HibernateUtil;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by Rayest on 2016/8/13 0013.
  */
 public class StudentTest {
-    @Test
-    public void testStudent(){
-        SessionFactory sessionFactory = null;
-        Configuration configuration = new Configuration().configure();
-        sessionFactory = configuration.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build());
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
 
-        Student student = new Student();
-        student.setName("Ray");
-        session.save(student);
+    @Before
+    public void init() {
+        sessionFactory = HibernateUtil.getSessionFactory();
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+    }
 
+    @After
+    public void after() {
         transaction.commit();
         session.close();
         sessionFactory.close();
-
     }
+
+    @Test
+    public void testAddStudent() {
+        Student student = new Student();
+        student.setName("Lee");
+        session.save(student);
+    }
+
+    @Test
+    public void testDeleteStudent() {
+        Student student = (Student) session.get(Student.class, Long.valueOf(1));
+        session.delete(student);
+    }
+
+    @Test
+    public void testUpdateStudent() {
+        Student student = (Student) session.get(Student.class, Long.valueOf(5));
+        student.setName("Paris");
+        session.save(student);
+    }
+
+    @Test
+    public void testListStudent() {
+        String hql = "from Student";
+        Query query = session.createQuery(hql);
+        List<Student> students = query.list();
+        for (Student student : students){
+            System.out.println(student);
+        }
+    }
+
+
 }
